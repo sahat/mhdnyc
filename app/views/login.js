1236393855492a99cef5512f336ca53e8003d053
Sahat Yalkabov <sakhat@gmail.com>
2013-10-07 17:07:37 -0400

    Redrect to homepage on successful sign in

--- app/views/login.js
+++ app/views/login.js
@@ -15,7 +15,20 @@ define([
     },
 
     initialize: function() {
-
+      function signinCallback(authResult) {
+        if (authResult['access_token']) {
+          // Update the app to reflect a signed in user
+          // Hide the sign-in button now that the user is authorized, for example:
+          document.getElementById('signinButton').setAttribute('style', 'display: none');
+        } else if (authResult['error']) {
+          // Update the app to reflect a signed out user
+          // Possible error values:
+          //   "user_signed_out" - User is signed-out
+          //   "access_denied" - User denied access to your app
+          //   "immediate_failed" - Could not automatically log in the user
+          console.log('Sign-in state: ' + authResult['error']);
+        }
+      }
     },
 
     render: function() {
